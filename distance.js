1f5cf823e44965b79ac3de4ed1a2593799c01586
Sean Falese <Sean@Seans-MBP.fios-router.home>
2020-06-25 23:04:22 -0400

    commit of all components

---
+++ distance.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+const apiKey ="WkBNkwQFQHCA6CF860bKzC6vhsTI4L87Tpn9dXZPvShRKuJB96yoNdd29qQ7kw8g";
+const zipCodeURL = 'https://www.zipcodeapi.com/rest/';
+
+var distance = {
+    find: function(req, res, next) {
+        +'/distance.json/' + req.params.zipcode1 + '/'
+        + req.params.zipcode2 + '/mile',
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          response=JSON.parse(body);
+          res.send(response);
+
+        } else {
+          console.log(response.statusCode + response.body);
+          res.send({distance: -1});
+        }
+      });
+    }
+};
+
+module.exports = distance;
